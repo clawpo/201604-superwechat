16dcfe334771e6d4ac375b7c101d5663fee8302a
耿小泰 <clawpo@foxmail.com>
2016-07-19 16:59:41 +0800

    添加okhttp工具类以及jar包

--- src/cn/ucai/superwechat/I.java
+++ src/cn/ucai/superwechat/I.java
@@ -81,6 +81,7 @@ public interface I {
 	String AVATAR_TYPE_GROUP_PATH 					=		"group_icon";					//群组头像保存目录
 	String AVATAR_SUFFIX_PNG						=		".png";							//PNG图片后缀名
 	String AVATAR_SUFFIX_JPG						=		".jpg";							//JPG图片后缀名
+    String QUESTION                                 =       "?";                            //问号
 	int LOCATION_IS_SEARCH_ALLOW					=		1;								//可以被搜索到地理位置
 	int LOCATION_IS_SEARCH_INHIBIT					=		0;								//禁止被搜索到地理位置
 	int MSG_SUCCESS						            =  		0;							    //默认成功
