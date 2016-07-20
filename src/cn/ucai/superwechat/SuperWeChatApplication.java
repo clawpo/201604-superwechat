2f64cd0e5b620a7ec43d08235a0e2545cdc93887
耿小泰 <clawpo@foxmail.com>
2016-07-20 17:06:36 +0800

    下载好友集合到全局变量

--- src/cn/ucai/superwechat/SuperWeChatApplication.java
+++ src/cn/ucai/superwechat/SuperWeChatApplication.java
@@ -18,6 +18,8 @@ import android.content.Context;
 
 import com.easemob.EMCallBack;
 
+import java.util.List;
+
 import cn.ucai.superwechat.bean.UserAvatar;
 
 public class SuperWeChatApplication extends Application {
@@ -111,6 +113,8 @@ public class SuperWeChatApplication extends Application {
 	}
     /**全局的当前登录用户信息*/
     private UserAvatar user;
+    /**全局的当前登录用户的好友集合*/
+    private List<UserAvatar> userList;
 
     public UserAvatar getUser() {
         return user;
@@ -118,5 +122,13 @@ public class SuperWeChatApplication extends Application {
 
     public void setUser(UserAvatar user) {
         this.user = user;
+    }
+
+    public List<UserAvatar> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<UserAvatar> userList) {
+        this.userList = userList;
     }
 }
--- src/cn/ucai/superwechat/activity/LoginActivity.java
+++ src/cn/ucai/superwechat/activity/LoginActivity.java
@@ -46,6 +46,7 @@ import cn.ucai.superwechat.bean.UserAvatar;
 import cn.ucai.superwechat.data.OkHttpUtils2;
 import cn.ucai.superwechat.db.UserDao;
 import cn.ucai.superwechat.domain.User;
+import cn.ucai.superwechat.task.DownloadContactListTask;
 import cn.ucai.superwechat.utils.CommonUtils;
 import cn.ucai.superwechat.utils.Utils;
 
@@ -227,6 +228,7 @@ public class LoginActivity extends BaseActivity {
         SuperWeChatApplication.getInstance().setUser(user);
         SuperWeChatApplication.currentUserNick = user.getMUserNick();
 
+        new DownloadContactListTask(LoginActivity.this,currentUsername).execute();
         try {
             // ** 第一次登录或者之前logout后再登录，加载所有本地群和回话
             // ** manually load all local groups and
