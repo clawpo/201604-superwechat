9493f84b8f794a2fb7353daa5ca9ba9584d09c1b
耿小泰 <clawpo@foxmail.com>
2016-07-29 17:14:32 +0800

    添加实体类

---
+++ src/cn/ucai/fulicenter/bean/CategoryGroupBean.java
@@ -0,0 +1,43 @@
+package cn.ucai.fulicenter.bean;
+
+import java.io.Serializable;
+
+/**
+ * Created by clawpo on 16/7/29.
+ */
+public class CategoryGroupBean implements Serializable {
+
+    /**
+     * id : 334
+     * name : 配饰
+     * imageUrl : muying/Jewelry.png
+     */
+
+    private int id;
+    private String name;
+    private String imageUrl;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
---
+++ src/cn/ucai/fulicenter/bean/MessageBean.java
@@ -0,0 +1,39 @@
+package cn.ucai.fulicenter.bean;
+
+/**
+ * Created by clawpo on 16/7/29.
+ */
+public class MessageBean {
+
+    /**
+     * success : true
+     * msg : 添加收藏成功
+     */
+
+    private boolean success;
+    private String msg;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return "MessageBean{" +
+                "success=" + success +
+                ", msg='" + msg + '\'' +
+                '}';
+    }
+}
