e3262fd39ff5e101273dd1d309fd56a48b0c03a2
耿小泰 <clawpo@foxmail.com>
2016-08-01 14:37:47 +0800

    创建新的mainactivity，修改闪屏图片

---
+++ src/cn/ucai/fulicenter/activity/FuliCenterMainActivity.java
@@ -0,0 +1,16 @@
+package cn.ucai.fulicenter.activity;
+
+import android.os.Bundle;
+
+import cn.ucai.fulicenter.R;
+
+/**
+ * Created by clawpo on 16/8/1.
+ */
+public class FuliCenterMainActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fulicetner_main);
+    }
+}
