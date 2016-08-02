e7613a3ede94b60240ac07df3c08625f178bffa4
耿小泰 <clawpo@foxmail.com>
2016-08-02 14:45:47 +0800

    新品fragment的上拉，下拉刷新操作

--- src/cn/ucai/fulicenter/activity/NewGoodFragment.java
+++ src/cn/ucai/fulicenter/activity/NewGoodFragment.java
@@ -11,6 +11,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,8 +36,9 @@ public class NewGoodFragment extends Fragment {
     RecyclerView mRecyclerView;
     GridLayoutManager mGridLayoutManager;
     GoodAdapter mAdapter;
+    TextView tvHint;
 
-    int pageId = 1;
+    int pageId = 0;
 
     @Nullable
     @Override
@@ -45,7 +48,53 @@ public class NewGoodFragment extends Fragment {
         mGoodList = new ArrayList<NewGoodBean>();
         initView(layout);
         initData();
+        setListener();
         return layout;
+    }
+
+    private void setListener() {
+        setPullDownRefreshListener();
+        setPullUpRefreshListener();
+    }
+
+    private void setPullDownRefreshListener() {
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                pageId = 0;
+                mSwipeRefreshLayout.setRefreshing(true);
+                tvHint.setVisibility(View.VISIBLE);
+                initData();
+            }
+        });
+    }
+
+    private void setPullUpRefreshListener() {
+        mRecyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            int lastItemPosition;
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                int a = RecyclerView.SCROLL_STATE_DRAGGING;//1
+                int b = RecyclerView.SCROLL_STATE_IDLE;//0
+                int c = RecyclerView.SCROLL_STATE_SETTLING;//2
+                Log.e(TAG,"newState="+newState);
+                if(newState==RecyclerView.SCROLL_STATE_IDLE
+                        && lastItemPosition==mAdapter.getItemCount()-1){
+                    pageId += I.PAGE_SIZE_DEFAULT;
+                    initData();
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                int f = mGridLayoutManager.findFirstVisibleItemPosition();
+                int l = mGridLayoutManager.findLastVisibleItemPosition();
+                Log.e(TAG,"f="+f+",l="+l);
+                lastItemPosition = mGridLayoutManager.findLastVisibleItemPosition();
+            }
+        });
     }
 
     private void initData() {
@@ -53,6 +102,8 @@ public class NewGoodFragment extends Fragment {
             findNewGoodList(new OkHttpUtils2.OnCompleteListener<NewGoodBean[]>() {
                 @Override
                 public void onSuccess(NewGoodBean[] result) {
+                    mSwipeRefreshLayout.setRefreshing(false);
+                    tvHint.setVisibility(View.GONE);
                     Log.e(TAG,"result="+result);
                     if(result!=null){
                         Log.e(TAG,"result.length="+result.length);
@@ -64,6 +115,9 @@ public class NewGoodFragment extends Fragment {
                 @Override
                 public void onError(String error) {
                     Log.e(TAG,"error="+error);
+                    mSwipeRefreshLayout.setRefreshing(false);
+                    tvHint.setVisibility(View.GONE);
+                    Toast.makeText(mContext,error,Toast.LENGTH_SHORT).show();
                 }
             });
         } catch (Exception e) {
@@ -97,7 +151,7 @@ public class NewGoodFragment extends Fragment {
         mRecyclerView.setLayoutManager(mGridLayoutManager);
         mAdapter = new GoodAdapter(mContext,mGoodList);
         mRecyclerView.setAdapter(mAdapter);
-
+        tvHint = (TextView) layout.findViewById(R.id.tv_refresh_hint);
     }
 
 }
