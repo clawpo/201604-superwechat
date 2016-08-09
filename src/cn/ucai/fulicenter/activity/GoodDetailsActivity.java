922e6cc5432b0426fbb57a79567a4465a65d2823
耿小泰 <clawpo@foxmail.com>
2016-08-09 14:51:20 +0800

    添加ShareSDK分享功能

--- src/cn/ucai/fulicenter/activity/GoodDetailsActivity.java
+++ src/cn/ucai/fulicenter/activity/GoodDetailsActivity.java
@@ -10,6 +10,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import cn.ucai.fulicenter.D;
 import cn.ucai.fulicenter.DemoHXSDKHelper;
 import cn.ucai.fulicenter.FuliCenterApplication;
@@ -62,6 +64,7 @@ public class GoodDetailsActivity extends BaseActivity {
     private void setListener() {
         MyOnClickListener listener = new MyOnClickListener();
         ivCollect.setOnClickListener(listener);
+        ivShare.setOnClickListener(listener);
     }
 
     private void initData() {
@@ -188,6 +191,9 @@ public class GoodDetailsActivity extends BaseActivity {
                 case R.id.iv_good_collect:
                     goodColect();
                     break;
+                case R.id.iv_good_share:
+                    showShare();
+                    break;
             }
         }
     }
@@ -266,5 +272,34 @@ public class GoodDetailsActivity extends BaseActivity {
         }else{
             ivCollect.setImageResource(R.drawable.bg_collect_in);
         }
+    }
+
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+        // 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle(getString(R.string.share));
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl(mGoodDetail.getShareUrl());
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText(mGoodDetail.getGoodsName());
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl(mGoodDetail.getShareUrl());
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment(mGoodDetail.getGoodsName());
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl(mGoodDetail.getShareUrl());
+
+        // 启动分享GUI
+        oks.show(this);
     }
 }
