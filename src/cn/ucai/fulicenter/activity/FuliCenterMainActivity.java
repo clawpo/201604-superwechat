fa5063c2cfd72cb092cf06f54dafd2ab39dce17d
耿小泰 <clawpo@foxmail.com>
2016-09-26 15:10:04 +0800

    删除主页中不需要的代码,修改布局文件,删除最后两个底部菜单

--- src/cn/ucai/fulicenter/activity/FuliCenterMainActivity.java
+++ src/cn/ucai/fulicenter/activity/FuliCenterMainActivity.java
@@ -1,20 +1,13 @@
 package cn.ucai.fulicenter.activity;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
-import android.widget.TextView;
 
-import cn.ucai.fulicenter.DemoHXSDKHelper;
 import cn.ucai.fulicenter.R;
-import cn.ucai.fulicenter.utils.Utils;
 
 /**
  * Created by clawpo on 16/8/1.
@@ -24,16 +17,11 @@ public class FuliCenterMainActivity extends BaseActivity {
     RadioButton rbNowGood;
     RadioButton rbBoutique;
     RadioButton rbCategory;
-    RadioButton rbCart;
-    RadioButton rbPersonalCenter;
-    TextView tvCartHint;
     RadioButton[] mrbTabs;
 
     NewGoodFragment mNewGoodFragment;
     BoutiqueFragment mBoutiqueFragment;
     CategoryFragment mCategoryFragment;
-    PersonalCenterFragment mPersonalCenterFragment;
-    CartFragment mCartFragment;
     Fragment[] mFragment;
 
     int index;
@@ -42,15 +30,12 @@ public class FuliCenterMainActivity extends BaseActivity {
     public static final int ACTION_LOGIN_PERSONAL = 100;
     public static final int ACTION_LOGIN_CART = 200;
 
-    updateCartNumReceiver mReceiver;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fulicetner_main);
         initView();
         initFragment();
-        setListener();
         // 添加显示第一个fragment
         getSupportFragmentManager()
                 .beginTransaction()
@@ -62,37 +47,24 @@ public class FuliCenterMainActivity extends BaseActivity {
                 .commit();
     }
 
-    private void setListener() {
-        setUpdateCartCountListener();
-    }
-
     private void initFragment() {
         mNewGoodFragment = new NewGoodFragment();
         mBoutiqueFragment = new BoutiqueFragment();
         mCategoryFragment = new CategoryFragment();
-        mCartFragment = new CartFragment();
-        mPersonalCenterFragment = new PersonalCenterFragment();
-        mFragment = new Fragment[5];
+        mFragment = new Fragment[3];
         mFragment[0] = mNewGoodFragment;
         mFragment[1] = mBoutiqueFragment;
         mFragment[2] = mCategoryFragment;
-        mFragment[3] = mCartFragment;
-        mFragment[4] = mPersonalCenterFragment;
     }
 
     private void initView() {
         rbNowGood = (RadioButton) findViewById(R.id.layout_new_good);
         rbBoutique = (RadioButton) findViewById(R.id.layout_boutique);
         rbCategory = (RadioButton) findViewById(R.id.layout_category);
-        rbCart = (RadioButton) findViewById(R.id.layout_cart);
-        rbPersonalCenter = (RadioButton) findViewById(R.id.layout_personal_center);
-        tvCartHint = (TextView) findViewById(R.id.tvCartHint);
-        mrbTabs = new RadioButton[5];
+        mrbTabs = new RadioButton[3];
         mrbTabs[0] = rbNowGood;
         mrbTabs[1] = rbBoutique;
         mrbTabs[2] = rbCategory;
-        mrbTabs[3] = rbCart;
-        mrbTabs[4] = rbPersonalCenter;
 
     }
 
@@ -106,20 +78,6 @@ public class FuliCenterMainActivity extends BaseActivity {
                 break;
             case R.id.layout_category:
                 index = 2;
-                break;
-            case R.id.layout_cart:
-                if(DemoHXSDKHelper.getInstance().isLogined()) {
-                    index = 3;
-                }else{
-                    gotoLogin(ACTION_LOGIN_CART);
-                }
-                break;
-            case R.id.layout_personal_center:
-                if(DemoHXSDKHelper.getInstance().isLogined()) {
-                    index = 4;
-                }else{
-                    gotoLogin(ACTION_LOGIN_PERSONAL);
-                }
                 break;
         }
         Log.e(TAG,"index="+index+",currentIndex="+currentIndex);
@@ -140,10 +98,6 @@ public class FuliCenterMainActivity extends BaseActivity {
         }
     }
 
-    private void gotoLogin(int action) {
-        startActivityForResult(new Intent(this,LoginActivity.class),action);
-    }
-
     private void setRadioButtonStatus(int index) {
         for(int i=0;i<mrbTabs.length;i++){
             if(index==i){
@@ -155,63 +109,9 @@ public class FuliCenterMainActivity extends BaseActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Log.e(TAG,"onActivityResult");
-        if(DemoHXSDKHelper.getInstance().isLogined()) {
-            if(requestCode==ACTION_LOGIN_PERSONAL){
-                index = 4;
-            }
-            if(requestCode==ACTION_LOGIN_CART){
-                index = 3;
-            }
-        }
-    }
-
-    @Override
     protected void onResume() {
         super.onResume();
-        Log.e(TAG,"onResume");
-        if(!DemoHXSDKHelper.getInstance().isLogined() && index == 4) {
-            index = 0;
-        }
         setFragment();
         setRadioButtonStatus(currentIndex);
-        updateCartNum();
-    }
-
-    class updateCartNumReceiver extends BroadcastReceiver{
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            updateCartNum();
-        }
-    }
-
-    private void setUpdateCartCountListener(){
-        mReceiver = new updateCartNumReceiver();
-        IntentFilter filter = new IntentFilter("update_cart_list");
-        filter.addAction("update_user");
-        registerReceiver(mReceiver,filter);
-    }
-
-    private void updateCartNum() {
-        int count = Utils.sumCartCount();
-        Log.e(TAG,"count="+count);
-        if(!DemoHXSDKHelper.getInstance().isLogined() || count ==0){
-            tvCartHint.setText(String.valueOf(0));
-            tvCartHint.setVisibility(View.GONE);
-        }else{
-            tvCartHint.setText(String.valueOf(count));
-            tvCartHint.setVisibility(View.VISIBLE);
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if(mReceiver!=null){
-            unregisterReceiver(mReceiver);
-        }
     }
 }
